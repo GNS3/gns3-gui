10ac6a998d9739fe9dd680aa531757c976818b91
Massimiliano Pippi <mpippi@gmail.com>
2014-04-23 12:32:34 +0200

    added gui tests folder

---
+++ tests/test_preferences_dlg.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from PyQt4.QtTest import QTest
+
+
+class TestCloudPreferencesPage(unittest.TestCase):
+    def test_foo(self):
+        self.assertTrue(True)
