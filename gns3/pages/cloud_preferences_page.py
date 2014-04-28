fb5ce128f43f4ecf6621a175e03c73f6192bf20d
Massimiliano Pippi <mpippi@gmail.com>
2014-04-28 14:49:07 +0200

    added controls and logic to cloud prefs page

--- gns3/pages/cloud_preferences_page.py
+++ gns3/pages/cloud_preferences_page.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from ..ui.cloud_preferences_page_ui import Ui_CloudPreferencesPageWidget
+from ..settings import CLOUD_PROVIDERS, CLOUD_REGIONS
 
 from PyQt4 import QtGui
 
@@ -12,6 +13,19 @@ class CloudPreferencesPage(QtGui.QWidget, Ui_CloudPreferencesPageWidget):
 
         QtGui.QWidget.__init__(self)
         self.setupUi(self)
+
+        # fill providers combo box
+        self.provider_index_id = [""]
+        self.uiCloudProviderComboBox.addItem("Select provider...")
+        for k, v in CLOUD_PROVIDERS.items():
+            self.uiCloudProviderComboBox.addItem(v)
+            self.provider_index_id.append(k)
+
+        # fill region combo box
+        self.region_index_id = []
+        for k, v in CLOUD_REGIONS.items():
+            self.uiRegionComboBox.addItem(v)
+            self.region_index_id.append(k)
 
         from ..main_window import MainWindow
         self.settings = MainWindow.instance().cloud_settings()
@@ -46,7 +60,14 @@ class CloudPreferencesPage(QtGui.QWidget, Ui_CloudPreferencesPageWidget):
             # do not select any radio button the first time
             pass
         elif do_store_api_key:
+            # populate all the cloud stuff
             self.uiRememberAPIKeyRadioButton.setChecked(True)
+            cloud_p = self.settings['cloud_provider']
+            if cloud_p:
+                self.uiCloudProviderComboBox.setCurrentIndex(self.provider_index_id.index(cloud_p))
+            self.uiRegionComboBox.setCurrentIndex(
+                self.region_index_id.index(self.settings['cloud_region']))
+
         else:
             self.uiForgetAPIKeyRadioButton.setChecked(True)
 
@@ -61,11 +82,17 @@ class CloudPreferencesPage(QtGui.QWidget, Ui_CloudPreferencesPageWidget):
             if self._store_api_key():
                 self.settings['cloud_user_name'] = self.uiUserNameLineEdit.text()
                 self.settings['cloud_api_key'] = self.uiAPIKeyLineEdit.text()
-                self.settings["cloud_store_api_key"] = True
+                self.settings['cloud_store_api_key'] = True
+                self.settings['cloud_provider'] = \
+                    self.provider_index_id[self.uiCloudProviderComboBox.currentIndex()]
+                self.settings['cloud_region'] = \
+                    self.region_index_id[self.uiRegionComboBox.currentIndex()]
             else:
                 self.settings['cloud_user_name'] = ""
                 self.settings['cloud_api_key'] = ""
-                self.settings["cloud_store_api_key"] = False
+                self.settings['cloud_store_api_key'] = False
+                del self.settings['cloud_provider']
+                del self.settings['cloud_region']
 
             if not self.settings['cloud_store_api_key_chosen']:
                 # user made a choice
