5ef1b71ba04cb527498a78c989312f3255ae0cda
Massimiliano Pippi <mpippi@gmail.com>
2014-05-02 15:16:08 +0200

    changed parameter name

--- gns3/pages/cloud_preferences_page.py
+++ gns3/pages/cloud_preferences_page.py
@@ -149,7 +149,7 @@ class CloudPreferencesPage(QtGui.QWidget, Ui_CloudPreferencesPageWidget):
                 self.settings['cloud_store_api_key_chosen'] = True
 
             from ..main_window import MainWindow
-            MainWindow.instance().setCloudSettings(self.settings, store=self._store_api_key())
+            MainWindow.instance().setCloudSettings(self.settings, persist=self._store_api_key())
 
             return True
         return False
