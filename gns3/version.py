ba0fdbb70e03db06c2dc86a406781dac5653eb86
grossmj <grossmj@gns3.net>
2014-05-21 23:32:57 -0600

    Reactivate cloud preferences page after alpha5.

--- gns3/version.py
+++ gns3/version.py
@@ -25,5 +25,5 @@ or negative for a release candidate or beta (after the base version
 number has been incremented)
 """
 
-__version__ = "1.0a5"
+__version__ = "1.0a6.dev1"
 __version_info__ = (1, 0, 0, -99)
