549e75c5fefbc3bea11e2f7b10947a12a81edaeb
Massimiliano Pippi <mpippi@gmail.com>
2014-05-27 17:22:19 +0200

    moved into init module some utils

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,6 +6,18 @@ from PyQt4.QtGui import QApplication
 import sys
 
 
+def make_getitem(container):
+    def getitem(name):
+        return container[name]
+    return getitem
+
+
+def make_setitem(container):
+    def setitem(name, val):
+        container[name] = val
+    return setitem
+
+
 class BaseTest(TestCase):
     """
     Base class for all the tests
