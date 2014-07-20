eb23d95ce36be88d57b6fa20d07675f30b6f5526
Massimiliano Pippi <mpippi@gmail.com>
2014-07-20 12:01:48 +0200

    store cloud instances into topology

--- gns3/topology.py
+++ gns3/topology.py
@@ -21,6 +21,7 @@ Handles the saving and loading of a topology.
 """
 
 import os
+from collections import namedtuple
 
 from .qt import QtCore, QtGui, QtSvg
 from .items.node_item import NodeItem
@@ -39,6 +40,8 @@ from pkg_resources import parse_version
 import logging
 log = logging.getLogger(__name__)
 
+TopologyInstance = namedtuple("TopologyInstance", ["name", "size_id", "image_id"], verbose=False)
+
 
 class Topology(object):
     """
@@ -56,6 +59,7 @@ class Topology(object):
         self._topology = None
         self._initialized_nodes = []
         self._resources_type = "local"
+        self._instances = []
 
     def addNode(self, node):
         """
@@ -197,6 +201,26 @@ class Topology(object):
         if image in self._images:
             self._images.remove(image)
 
+    def addInstance(self, name, size_id, image_id):
+        """
+        Add an instance to this cloud topology
+        """
+
+        i = TopologyInstance(name=name, size_id=size_id, image_id=image_id)
+        self._instances.append(i)
+
+    def removeInstance(self, name):
+        """
+        Removes an instance from this cloud topology
+
+        :param name: the name of the instance
+        """
+
+        for instance in self._instances:
+            if instance.name == name:
+                self._instances.remove(instance)
+                break
+
     def nodes(self):
         """
         Returns all the nodes in this topology.
@@ -239,6 +263,13 @@ class Topology(object):
 
         return self._images
 
+    def instances(self):
+        """
+        Returns the list of instances for this cloud topology
+        """
+
+        return self._instances
+
     def reset(self):
         """
         Resets this topology.
@@ -253,6 +284,7 @@ class Topology(object):
         self._images.clear()
         self._initialized_nodes.clear()
         self._resources_type = "local"
+        self._instances = []
         log.info("topology has been reset")
 
     def _dump_gui_settings(self, topology):
@@ -367,6 +399,16 @@ class Topology(object):
                 log.info("saving server {}:{}".format(server.host, server.port))
                 topology_servers.append(server.dump())
 
+        # instances
+        if self._resources_type == "cloud":
+            topology_instances = topology["topology"]["instances"] = []
+            for i in self._instances:
+                log.info("saving cloud instance {}".format(i.name))
+                topology_instances.append({
+                    "name": i.name,
+                    "size_id": i.size_id,
+                    "image_id": i.image_id
+                })
         if include_gui_data:
             self._dump_gui_settings(topology)
 
@@ -559,6 +601,12 @@ class Topology(object):
                 image_item.load(topology_image)
                 view.scene().addItem(image_item)
                 self.addImage(image_item)
+
+        # instances
+        if "instances" in topology["topology"]:
+            instances = topology["topology"]["instances"]
+            for instance in instances:
+                self.addInstance(instance["name"], instance["size_id"], instance["image_id"])
 
         if topology_file_errors:
             errors = "\n".join(topology_file_errors)
--- tests/test_topology.py
+++ tests/test_topology.py
@@ -10,7 +10,7 @@ class TestTopology(TestCase):
         self.t = Topology.instance()
 
     def tearDown(self):
-        del self.t
+        self.t.reset()
 
     def test_resources_type(self):
         self.assertEqual(self.t._resources_type, 'local')
@@ -27,7 +27,6 @@ class TestTopology(TestCase):
         self.assertEqual(topology['resources_type'], 'cloud')
 
     def test_resources_type_load(self):
-        self.t.reset()
         self.assertEqual(self.t.resourcesType, 'local')  # default value
         topology = {
             'project_type': 'cloud',
@@ -51,3 +50,48 @@ class TestTopology(TestCase):
         self.assertEqual(len(self.t._nodes), 0)
         self.assertEqual(len(self.t._initialized_nodes), 0)
         self.assertEqual(self.t._resources_type, 'local')
+
+    def test_instances(self):
+        self.assertEqual(self.t._instances, [])
+        self.t.addInstance(name="My instance", size_id="123", image_id="1234567890")
+        self.assertEqual(len(self.t._instances), 1)
+        self.t.removeInstance("My fake instance")
+        self.assertEqual(len(self.t._instances), 1)
+        self.t.removeInstance("My instance")
+        self.assertEqual(self.t._instances, [])
+
+    def test_instances_dumped(self):
+        test_settings = {'project_type': 'cloud'}
+        MainWindow.instance() ._project_settings.update(test_settings)
+        self.t.addInstance(name="My instance", size_id="123", image_id="1234567890")
+        topology = self.t.dump()
+        self.assertIn('instances', topology["topology"])
+        instances = topology["topology"]["instances"]
+        self.assertEqual(len(instances), 1)
+        self.assertEqual(instances.pop()["name"], "My instance")
+
+    def test_instaces_load(self):
+        topology = {
+            'project_type': 'cloud',
+            'type': 'topology',
+            'version': '3.0',
+            'topology': {
+                'instances': [
+                    {
+                        'name': 'Foo Instance',
+                        'size_id': 'id1',
+                        'image_id': 'id2',
+                    },
+                    {
+                        'name': 'Another Foo Instance',
+                        'size_id': 'id123',
+                        'image_id': 'id234',
+                    }
+                ]
+            }
+        }
+        self.t.load(topology)
+        instances = self.t.instances()
+        self.assertEqual(len(instances), 2)
+        self.assertEqual(instances[0].name, 'Foo Instance')
+        self.assertEqual(instances[1].name, 'Another Foo Instance')
