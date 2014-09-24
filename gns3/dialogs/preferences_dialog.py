f74dc93336f8367f8d03441e513d9824ac605e23
grossmj <grossmj@gns3.net>
2014-09-24 11:14:28 -0600

    Bump to version 1.0-beta1.dev1.

--- gns3/dialogs/preferences_dialog.py
+++ gns3/dialogs/preferences_dialog.py
@@ -58,7 +58,7 @@ class PreferencesDialog(QtGui.QDialog, Ui_PreferencesDialog):
             GeneralPreferencesPage,
             ServerPreferencesPage,
             PacketCapturePreferencesPage,
-            #CloudPreferencesPage, #FIXME: hide for beta3
+            CloudPreferencesPage,
         ]
 
         for page in pages:
--- gns3/version.py
+++ gns3/version.py
@@ -25,5 +25,5 @@ or negative for a release candidate or beta (after the base version
 number has been incremented)
 """
 
-__version__ = "1.0beta3"
+__version__ = "1.0beta4.dev1"
 __version_info__ = (1, 0, 0, -99)
