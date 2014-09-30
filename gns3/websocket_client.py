0942b0fae1b98854426464f4a128d358f525e233
grossmj <grossmj@gns3.net>
2014-09-30 11:42:20 -0600

    Fix missing paramiko module in setup.py and disable broken server version check.

--- gns3/websocket_client.py
+++ gns3/websocket_client.py
@@ -129,7 +129,7 @@ class WebSocketClient(WebSocketBaseClient):
         self.cookie_processor = urllib.request.HTTPCookieProcessor()
         self.opener = urllib.request.build_opener(self.https_handler, self.cookie_processor)
 
-        self.check_server_version()
+        #self.check_server_version()
         self._connect()
 
     def _connect(self):
