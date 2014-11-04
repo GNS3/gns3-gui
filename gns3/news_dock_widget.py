61317fced0e758efa6b95b86cd92a365f38d7b46
Jeremy <grossmj@gns3.net>
2014-11-04 14:17:33 -0700

    New news dock widget.

--- gns3/news_dock_widget.py
+++ gns3/news_dock_widget.py
@@ -46,7 +46,7 @@ class NewsDockWidget(QtGui.QDockWidget, Ui_NewsDockWidget):
         self._timer.timeout.connect(self._loadFinishedSlot)
         self._timer.setSingleShot(True)
         self._timer.start(5000)
-        self.uiWebView.load(QtCore.QUrl("http://as.gns3.com/software/docked.html"))
+        self.uiWebView.load(QtCore.QUrl("http://as.gns3.com/software/docked_200x200.html"))
 
     def _refreshSlot(self):
         """
--- gns3/ui/news_dock_widget_ui.py
+++ gns3/ui/news_dock_widget_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/grossmj/PycharmProjects/gns3-gui/gns3/ui/news_dock_widget.ui'
 #
-# Created: Sat Oct 18 15:38:49 2014
+# Created: Tue Nov  4 11:41:22 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,7 +26,7 @@ except AttributeError:
 class Ui_NewsDockWidget(object):
     def setupUi(self, NewsDockWidget):
         NewsDockWidget.setObjectName(_fromUtf8("NewsDockWidget"))
-        NewsDockWidget.resize(300, 275)
+        NewsDockWidget.resize(203, 225)
         NewsDockWidget.setFloating(False)
         NewsDockWidget.setFeatures(QtGui.QDockWidget.DockWidgetFloatable|QtGui.QDockWidget.DockWidgetMovable)
         self.dockWidgetContents = QtGui.QWidget()
@@ -34,11 +34,11 @@ class Ui_NewsDockWidget(object):
         self.horizontalLayout = QtGui.QHBoxLayout(self.dockWidgetContents)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setSizeConstraint(QtGui.QLayout.SetFixedSize)
-        self.horizontalLayout.setMargin(0)
+        self.horizontalLayout.setContentsMargins(1, 0, 2, 0)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         self.uiWebView = QtWebKit.QWebView(self.dockWidgetContents)
-        self.uiWebView.setMinimumSize(QtCore.QSize(300, 250))
-        self.uiWebView.setMaximumSize(QtCore.QSize(300, 250))
+        self.uiWebView.setMinimumSize(QtCore.QSize(200, 200))
+        self.uiWebView.setMaximumSize(QtCore.QSize(200, 200))
         self.uiWebView.setProperty("url", QtCore.QUrl(None))
         self.uiWebView.setObjectName(_fromUtf8("uiWebView"))
         self.horizontalLayout.addWidget(self.uiWebView)
@@ -48,6 +48,6 @@ class Ui_NewsDockWidget(object):
         QtCore.QMetaObject.connectSlotsByName(NewsDockWidget)
 
     def retranslateUi(self, NewsDockWidget):
-        NewsDockWidget.setWindowTitle(_translate("NewsDockWidget", "GNS3 Jungle", None))
+        NewsDockWidget.setWindowTitle(_translate("NewsDockWidget", "Jungle Newsfeed", None))
 
 from PyQt4 import QtWebKit
