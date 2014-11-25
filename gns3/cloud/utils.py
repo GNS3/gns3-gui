e1b81fb9318b8f2ad16440466ba26f7cc57ac7a6
Jeremy <grossmj@gns3.net>
2014-11-25 11:54:35 -0700

    Fixes PyQt module usage.

--- gns3/cloud/utils.py
+++ gns3/cloud/utils.py
@@ -287,7 +287,7 @@ class UploadProjectThread(QtCore.QThread):
     update = QtCore.pyqtSignal(int)
 
     def __init__(self, parent, cloud_settings, project_path, images_path):
-        super(QThread, self).__init__(parent)
+        super(QtCore.QThread, self).__init__(parent)
         self.cloud_settings = cloud_settings
         self.project_path = project_path
         self.images_path = images_path
@@ -385,7 +385,7 @@ class DownloadProjectThread(QtCore.QThread):
     update = QtCore.pyqtSignal(int)
 
     def __init__(self, parent, cloud_project_file_name, project_dest_path, images_dest_path, cloud_settings):
-        super(QThread, self).__init__(parent)
+        super(QtCore.QThread, self).__init__(parent)
         self.project_name = cloud_project_file_name
         self.project_dest_path = project_dest_path
         self.images_dest_path = images_dest_path
@@ -442,7 +442,7 @@ class DeleteProjectThread(QtCore.QThread):
     update = QtCore.pyqtSignal(int)
 
     def __init__(self, parent, project_file_name, cloud_settings):
-        super(QThread, self).__init__(parent)
+        super(QtCore.QThread, self).__init__(parent)
         self.project_file_name = project_file_name
         self.cloud_settings = cloud_settings
 
