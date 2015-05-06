0eef72b6e301158ef7ed11deb70cb54420dbc041
Julien Duponchelle <julien@duponchelle.info>
2015-05-06 10:47:03 +0200

    Fix broken pipe error on OSX when frozen
    Fix https://github.com/GNS3/gns3-server/issues/166

--- gns3/logger.py
+++ gns3/logger.py
@@ -74,6 +74,9 @@ class ColouredStreamHandler(logging.StreamHandler):
             stream.write(msg)
             stream.write(self.terminator)
             self.flush()
+        # On OSX when frozen flush raise a BrokenPipeError
+        except BrokenPipeError:
+            pass
         except Exception:
             self.handleError(record)
 
