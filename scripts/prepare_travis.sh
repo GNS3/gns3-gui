1a55c472e0facab4cb3d2b924a584fe85d8a411b
Julien Duponchelle <julien@duponchelle.info>
2015-06-05 17:57:42 +0200

    Fix PyQT download link

--- scripts/prepare_travis.sh
+++ scripts/prepare_travis.sh
@@ -49,7 +49,7 @@ echo "Install PyQT"
 # install PyQt
 if [ ! -d "PyQt-x11-gpl-${PYQT}" ]
 then
-    wget --quiet --output-document=pyqt.tar.gz "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-${PYQT}/PyQt-x11-gpl-${PYQT}.tar.gz"
+    wget --quiet --output-document=pyqt.tar.gz "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-${PYQT}/PyQt-gpl-${PYQT}.tar.gz"
     tar -xf pyqt.tar.gz
     cd "PyQt-x11-gpl-${PYQT}"
     python -B configure.py --confirm-license
