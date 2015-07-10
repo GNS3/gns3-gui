942bf9094d23003f1ad4d78d21ea57dfa1135538
Julien Duponchelle <julien@duponchelle.info>
2015-07-10 17:28:20 +0200

    Force UTF-8 when reading server configuration file
    Fix #525

--- gns3/local_server_config.py
+++ gns3/local_server_config.py
@@ -61,7 +61,7 @@ class LocalServerConfig:
         """
 
         try:
-            self._config.read(self._config_file)
+            self._config.read(self._config_file, encoding="utf-8")
         except (OSError, configparser.Error) as e:
             log.error("Could not read the local server configuration {}: {}".format(self._config_file, e))
 
