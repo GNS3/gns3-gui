b1658f3799b6efe19924f0d7014586298be51949
Jeremy <grossmj@gns3.net>
2015-07-27 12:03:07 -0600

    1.3.8

--- gns3/version.py
+++ gns3/version.py
@@ -25,5 +25,5 @@ or negative for a release candidate or beta (after the base version
 number has been incremented)
 """
 
-__version__ = "1.3.8dev1"
-__version_info__ = (1, 3, 8, -99)
+__version__ = "1.3.8"
+__version_info__ = (1, 3, 8, 0)
