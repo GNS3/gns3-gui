034c9e8ad518e166d6e992dc3011b3b968c9841a
Julien Duponchelle <julien@duponchelle.info>
2015-09-16 13:19:29 +0200

    Forgot to remove this code

--- gns3/main_window.py
+++ gns3/main_window.py
@@ -111,13 +111,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self._local_config_timer.start(1000)  # milliseconds
 
         self._uiNewsDockWidget = None
-        if not checkLicence():
-            try:
-                from .news_dock_widget import NewsDockWidget
-                self._uiNewsDockWidget = NewsDockWidget(self)
-                self.addDockWidget(QtCore.Qt.DockWidgetArea(QtCore.Qt.BottomDockWidgetArea), self._uiNewsDockWidget)
-            except ImportError:
-                pass
+        try:
+            from .news_dock_widget import NewsDockWidget
+            self._uiNewsDockWidget = NewsDockWidget(self)
+            self.addDockWidget(QtCore.Qt.DockWidgetArea(QtCore.Qt.BottomDockWidgetArea), self._uiNewsDockWidget)
+        except ImportError:
+            pass
 
         # restore the geometry and state of the main window.
         self.restoreGeometry(QtCore.QByteArray().fromBase64(self._settings["geometry"].encode()))
