1095ed16633ac9740549eb9cf1d09c2de6966f74
grossmj <grossmj@gns3.net>
2015-09-22 16:07:21 -0600

    Bump version to 1.4.0.dev9

--- gns3/version.py
+++ gns3/version.py
@@ -25,5 +25,5 @@ or negative for a release candidate or beta (after the base version
 number has been incremented)
 """
 
-__version__ = "1.4.0b3"
-__version_info__ = (1, 4, 0, -3)
+__version__ = "1.4.0.dev9"
+__version_info__ = (1, 4, 0, 9)
