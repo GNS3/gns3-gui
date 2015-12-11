8d4fc9585e87ecf22dd8791d2869f352dee1b14e
Julien Duponchelle <julien@duponchelle.info>
2015-12-11 09:22:46 +0100

    1.3.13

--- gns3/version.py
+++ gns3/version.py
@@ -25,6 +25,6 @@ or negative for a release candidate or beta (after the base version
 number has been incremented)
 """
 
-__version__ = "1.3.12"
-__version_info__ = (1, 3, 12, 0)
+__version__ = "1.3.13"
+__version_info__ = (1, 3, 13, 0)
 
