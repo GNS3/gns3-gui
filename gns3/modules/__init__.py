e5ae7b2d25c116574cf6fe4115420f45d4543917
Julien Duponchelle <julien@duponchelle.info>
2015-12-18 09:49:50 +0100

    Turn off Docker until 1.5

--- gns3/modules/__init__.py
+++ gns3/modules/__init__.py
@@ -22,6 +22,5 @@ from gns3.modules.vpcs import VPCS
 from gns3.modules.virtualbox import VirtualBox
 from gns3.modules.qemu import Qemu
 from gns3.modules.vmware import VMware
-from gns3.modules.docker import Docker
 
-MODULES = [VPCS, Dynamips, IOU, Qemu, VirtualBox, VMware, Docker, Builtin]
+MODULES = [VPCS, Dynamips, IOU, Qemu, VirtualBox, VMware, Builtin]
