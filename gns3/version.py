55216e1de70fa60ad12a64918959063c5700ab2b
grossmj <grossmj@gns3.net>
2016-02-19 16:02:51 -0700

    1.4.4dev1

--- gns3/version.py
+++ gns3/version.py
@@ -25,5 +25,5 @@ or negative for a release candidate or beta (after the base version
 number has been incremented)
 """
 
-__version__ = "1.4.3"
-__version_info__ = (1, 4, 3, 0)
+__version__ = "1.4.4dev1"
+__version_info__ = (1, 4, 4, -99)
