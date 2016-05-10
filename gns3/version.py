cee5fb915a20753a5a74b5ecb30b3c1aa6cac03d
Julien Duponchelle <julien@duponchelle.info>
2016-05-10 17:58:59 +0200

    Prepare 1.5 alpha 1

--- gns3/version.py
+++ gns3/version.py
@@ -25,5 +25,5 @@ or negative for a release candidate or beta (after the base version
 number has been incremented)
 """
 
-__version__ = "1.5.0dev2"
+__version__ = "1.5.0a1"
 __version_info__ = (1, 5, 0, -99)
