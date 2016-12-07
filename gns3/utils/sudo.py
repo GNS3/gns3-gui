5ab9f95379934d206ab7928db757c5eb14fd18d7
Julien Duponchelle <julien@gns3.net>
2016-12-07 16:45:01 +0100

    Use osascript on OSX for asking admin permission
    Fix #1699

--- gns3/utils/sudo.py
+++ gns3/utils/sudo.py
@@ -16,12 +16,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import shlex
 import subprocess
 
-from ..qt import QtWidgets
-from .progress_dialog import ProgressDialog
-from .wait_for_command_worker import WaitForCommandWorker
-from .wait_for_runas_worker import WaitForRunAsWorker
+from gns3.qt import QtWidgets
+from gns3.utils.progress_dialog import ProgressDialog
+from gns3.utils.wait_for_command_worker import WaitForCommandWorker
+from gns3.utils.wait_for_runas_worker import WaitForRunAsWorker
 
 import logging
 log = logging.getLogger(__name__)
@@ -37,9 +38,32 @@ def sudo(*commands, parent=None, shell=False):
         parent = MainWindow.instance()
 
     while True:
-        if not sys.platform.startswith("win32"):
+        if sys.platform.startswith("win32"):
+            for command in commands:
+                worker = WaitForRunAsWorker(command)
+                progress_dialog = ProgressDialog(worker, "Run as administrator", "Executing command...", "Cancel", busy=True, parent=parent)
+                progress_dialog.show()
+                if not progress_dialog.exec_():
+                    return False
+        elif sys.platform.startswith("darwin"):
+            cmd = []
+            for command in commands:
+                command = [shlex.quote(c) for c in command]
+                cmd.append(' '.join(command))
+            command = ["osascript", "-e", "do shell script \"{}\" with administrator privileges".format(' && '.join(cmd))]
+            worker = WaitForCommandWorker(command, shell=shell)
+
+            for line in worker.output().decode("utf-8", errors="ignore").splitlines():
+                log.info(line)
+            progress_dialog = ProgressDialog(worker, "Run as administrator", "Executing command...", "Cancel", busy=True, parent=parent)
+            progress_dialog.show()
+            if not progress_dialog.exec_():
+                return False
+
+        else:
             command_detail = []
             for command in commands:
+                command = [shlex.quote(c) for c in command]
                 command_detail.append(' '.join(command))
 
             password, ok = QtWidgets.QInputDialog.getText(parent,
@@ -70,11 +94,10 @@ def sudo(*commands, parent=None, shell=False):
                 progress_dialog.show()
                 if not progress_dialog.exec_():
                     return False
-        else:
-            for command in commands:
-                worker = WaitForRunAsWorker(command)
-                progress_dialog = ProgressDialog(worker, "Run as administrator", "Executing command...", "Cancel", busy=True, parent=parent)
-                progress_dialog.show()
-                if not progress_dialog.exec_():
-                    return False
         return True
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    main = QtWidgets.QMainWindow()
+    sudo(["touch", "/tmp/a"], ["touch", "/tmp/b"], parent=main)
