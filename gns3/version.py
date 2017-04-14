fd1a86df033f25e921f800f31917d1b5ecdfffc0
Julien Duponchelle <julien@gns3.net>
2017-04-14 09:00:57 +0200

    1.5.5dev1

--- gns3/version.py
+++ gns3/version.py
@@ -15,5 +15,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "1.5.4"
-__version_info__ = (1, 5, 4, 0)
+__version__ = "1.5.5dev1"
+__version_info__ = (1, 5, 5, -99)
