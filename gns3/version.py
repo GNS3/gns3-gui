a5435280d73742b123c8d00328589c85c8b01922
grossmj <grossmj@gns3.net>
2018-01-23 11:36:58 +0700

    Bump to version 2.2.0dev1 & refresh resources/ui files.

--- gns3/version.py
+++ gns3/version.py
@@ -16,8 +16,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = "2.1.1dev1"
-__version_info__ = (2, 1, 0, 0)
+__version__ = "2.2.0dev1"
+__version_info__ = (2, 2, 0, 99)
 
 # If it's a git checkout try to add the commit
 if "dev" in __version__:
