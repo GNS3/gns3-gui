d7b3f8d68dae856d4c45063609582e56f5097b61
ziajka <dominik@gns3.net>
2018-02-28 09:36:31 +0100

    Dialog with checkbox for including snapshots, Ref. gns3-server#1297

--- gns3/ui/snapshots_dialog_ui.py
+++ gns3/ui/snapshots_dialog_ui.py
@@ -1,41 +1,45 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/noplay/code/gns3/gns3-gui/gns3/ui/snapshots_dialog.ui'
+# Form implementation generated from reading ui file '/home/dominik/projects/gns3-gui-2.2/gns3/ui/snapshots_dialog.ui'
 #
-# Created by: PyQt5 UI code generator 5.4.2
+# Created by: PyQt5 UI code generator 5.10
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_SnapshotsDialog(object):
-
     def setupUi(self, SnapshotsDialog):
         SnapshotsDialog.setObjectName("SnapshotsDialog")
         SnapshotsDialog.setWindowModality(QtCore.Qt.WindowModal)
         SnapshotsDialog.resize(496, 288)
         self.gridLayout = QtWidgets.QGridLayout(SnapshotsDialog)
         self.gridLayout.setObjectName("gridLayout")
-        self.uiSnapshotsList = QtWidgets.QListWidget(SnapshotsDialog)
-        self.uiSnapshotsList.setObjectName("uiSnapshotsList")
-        self.gridLayout.addWidget(self.uiSnapshotsList, 0, 0, 1, 4)
         self.uiCreatePushButton = QtWidgets.QPushButton(SnapshotsDialog)
         self.uiCreatePushButton.setObjectName("uiCreatePushButton")
-        self.gridLayout.addWidget(self.uiCreatePushButton, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.uiCreatePushButton, 5, 0, 1, 1)
         self.uiRestorePushButton = QtWidgets.QPushButton(SnapshotsDialog)
         self.uiRestorePushButton.setEnabled(False)
         self.uiRestorePushButton.setObjectName("uiRestorePushButton")
-        self.gridLayout.addWidget(self.uiRestorePushButton, 1, 2, 1, 1)
+        self.gridLayout.addWidget(self.uiRestorePushButton, 5, 2, 1, 1)
         self.uiButtonBox = QtWidgets.QDialogButtonBox(SnapshotsDialog)
         self.uiButtonBox.setOrientation(QtCore.Qt.Horizontal)
         self.uiButtonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Close)
         self.uiButtonBox.setObjectName("uiButtonBox")
-        self.gridLayout.addWidget(self.uiButtonBox, 1, 3, 1, 1)
+        self.gridLayout.addWidget(self.uiButtonBox, 5, 3, 1, 1)
         self.uiDeletePushButton = QtWidgets.QPushButton(SnapshotsDialog)
         self.uiDeletePushButton.setEnabled(False)
         self.uiDeletePushButton.setObjectName("uiDeletePushButton")
-        self.gridLayout.addWidget(self.uiDeletePushButton, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.uiDeletePushButton, 5, 1, 1, 1)
+        self.uiSnapshotsList = QtWidgets.QListWidget(SnapshotsDialog)
+        self.uiSnapshotsList.setObjectName("uiSnapshotsList")
+        self.gridLayout.addWidget(self.uiSnapshotsList, 0, 0, 1, 4)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.uiIncludeSnapshots = QtWidgets.QCheckBox(SnapshotsDialog)
+        self.uiIncludeSnapshots.setObjectName("uiIncludeSnapshots")
+        self.verticalLayout.addWidget(self.uiIncludeSnapshots)
+        self.gridLayout.addLayout(self.verticalLayout, 2, 0, 1, 3)
 
         self.retranslateUi(SnapshotsDialog)
         self.uiButtonBox.accepted.connect(SnapshotsDialog.accept)
@@ -48,3 +52,5 @@ class Ui_SnapshotsDialog(object):
         self.uiCreatePushButton.setText(_translate("SnapshotsDialog", "&Create"))
         self.uiRestorePushButton.setText(_translate("SnapshotsDialog", "&Restore"))
         self.uiDeletePushButton.setText(_translate("SnapshotsDialog", "&Delete"))
+        self.uiIncludeSnapshots.setText(_translate("SnapshotsDialog", "Include previous snapshots in the snapshot"))
+
