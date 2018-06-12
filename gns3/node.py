9915beeb8ef4d8542ffb8d5e5c718b755188801f
grossmj <grossmj@gns3.net>
2018-06-12 17:45:54 +0800

    Use process name to bring console to front. Fixes #2514.

--- gns3/node.py
+++ gns3/node.py
@@ -22,7 +22,7 @@ import pathlib
 from gns3.controller import Controller
 from gns3.ports.ethernet_port import EthernetPort
 from gns3.ports.serial_port import SerialPort
-from gns3.utils.bring_to_front import bring_window_to_front_from_title
+from gns3.utils.bring_to_front import bring_window_to_front_from_process_name, bring_window_to_front_from_title
 from gns3.qt import QtGui, QtCore
 
 from .base_node import BaseNode
@@ -548,10 +548,18 @@ class Node(BaseNode):
         """
 
         if self.status() == Node.started:
-            if bring_window_to_front_from_title(self.name()):
-                return True
+            console_command = self.consoleCommand()
+            if console_command:
+                process_name = console_command.split()[0]
+                if bring_window_to_front_from_process_name(process_name, self.name()):
+                    return True
+                else:
+                    log.debug("Could not find process name '' and window title '{}' to bring it to front".format(process_name, self.name()))
             else:
-                log.debug("Could not find window title '{}' to bring it to front".format(self.name()))
+                if bring_window_to_front_from_title(self.name()):
+                    return True
+                else:
+                    log.debug("Could not find window title '{}' to bring it to front".format(self.name()))
         return False
 
     def setName(self, name):
