0150515338633cc519912126243e78a739048b4b
Jeremy Grossmann <grossmj@gns3.net>
2018-06-18 23:54:13 +0700

    Enable TraceNG module

--- gns3/modules/__init__.py
+++ gns3/modules/__init__.py
@@ -25,5 +25,4 @@ from gns3.modules.qemu import Qemu
 from gns3.modules.vmware import VMware
 from gns3.modules.docker import Docker
 
-#FIXME: removed TraceNG
-MODULES = [Builtin, VPCS, Dynamips, IOU, Qemu, VirtualBox, VMware, Docker]
+MODULES = [Builtin, VPCS, Dynamips, IOU, Qemu, VirtualBox, VMware, Docker, TraceNG]
