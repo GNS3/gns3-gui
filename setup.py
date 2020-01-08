626510865f6df474cb49877a940043de085208b9
grossmj <grossmj@gns3.net>
2020-01-09 04:19:21 +0800

    Update paths to icons for Linux

--- setup.py
+++ setup.py
@@ -41,16 +41,16 @@ class Tox(TestCommand):
 if sys.platform.startswith('linux'):
     data_files = [
         ("share/applications/", ["resources/linux/applications/gns3.desktop"]),
-        ("/usr/local/share/icons/hicolor/16x16/apps", ["resources/linux/icons/16x16/apps/gns3.png"]),
-        ("/usr/local/share/icons/hicolor/32x32/apps", ["resources/linux/icons/32x32/apps/gns3.png"]),
-        ("/usr/local/share/icons/hicolor/48x48/apps", ["resources/linux/icons/48x48/apps/gns3.png"]),
-        ("/usr/local/share/icons/hicolor/48x48/apps/mimetypes", ["resources/linux/icons/48x48/apps/mimetypes/application-x-gns3.png",
-                                                                 "resources/linux/icons/48x48/apps/mimetypes/application-x-gns3appliance.png",
-                                                                 "resources/linux/icons/48x48/apps/mimetypes/application-x-gns3project.png"]),
-        ("/usr/local/share/icons/hicolor/scalable/apps", ["resources/linux/icons/scalable/apps/gns3.svg"]),
-        ("/usr/local/share/icons/hicolor/scalable/apps/mimetypes", ["resources/linux/icons/scalable/apps/mimetypes/application-x-gns3.svg",
-                                                                    "resources/linux/icons/scalable/apps/mimetypes/application-x-gns3appliance.svg",
-                                                                    "resources/linux/icons/scalable/apps/mimetypes/application-x-gns3project.svg"]),
+        ("/usr/local/share/icons/hicolor/16x16/apps", ["resources/linux/icons/hicolor/16x16/apps/gns3.png"]),
+        ("/usr/local/share/icons/hicolor/32x32/apps", ["resources/linux/icons/hicolor/32x32/apps/gns3.png"]),
+        ("/usr/local/share/icons/hicolor/48x48/apps", ["resources/linux/icons/hicolor/48x48/apps/gns3.png"]),
+        ("/usr/local/share/icons/hicolor/48x48/apps/mimetypes", ["resources/linux/icons/hicolor/48x48/mimetypes/application-x-gns3.png",
+                                                                 "resources/linux/icons/hicolor/48x48/mimetypes/application-x-gns3appliance.png",
+                                                                 "resources/linux/icons/hicolor/48x48/mimetypes/application-x-gns3project.png"]),
+        ("/usr/local/share/icons/hicolor/scalable/apps", ["resources/linux/icons/hicolor/scalable/apps/gns3.svg"]),
+        ("/usr/local/share/icons/hicolor/scalable/apps/mimetypes", ["resources/linux/icons/hicolor/scalable/mimetypes/application-x-gns3.svg",
+                                                                    "resources/linux/icons/hicolor/scalable/mimetypes/application-x-gns3appliance.svg",
+                                                                    "resources/linux/icons/hicolor/scalable/mimetypes/application-x-gns3project.svg"]),
     ]
 else:
     data_files = []
