0f5f6ab645842a22ab8a877f7106848ea7b7f6ef
grossmj <grossmj@gns3.net>
2020-05-08 12:34:58 +0930

    Require setuptools>=17.1 in setup.py. Ref https://github.com/GNS3/gns3-server/issues/1751 This is to support environmental markers. https://github.com/pypa/setuptools/blob/master/CHANGES.rst#171

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
     include_package_data=True,
     package_data={"gns3": ["configs/*.txt", "schemas/*.json"]},
     platforms="any",
+    setup_requires=["setuptools>=17.1"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: X11 Applications :: Qt",
