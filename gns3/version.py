7b99ba325b846672a1a40609b390e263a8a108f0
grossmj <grossmj@gns3.net>
2020-08-07 21:12:46 +0930

    Development on 2.2.13dev1

--- gns3/version.py
+++ gns3/version.py
@@ -23,8 +23,8 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "2.2.12"
-__version_info__ = (2, 2, 12, 0)
+__version__ = "2.2.13dev1"
+__version_info__ = (2, 2, 13, 99)
 # If it's a git checkout try to add the commit
 if "dev" in __version__:
     try:
