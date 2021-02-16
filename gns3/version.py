8bc499c68f4168cd44387a4e47d890e23377e796
grossmj <grossmj@gns3.net>
2021-02-16 16:35:26 +1030

    Bump version to 2.2.18dev2

--- gns3/version.py
+++ gns3/version.py
@@ -23,7 +23,7 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "2.2.18dev1"
+__version__ = "2.2.18dev2"
 __version_info__ = (2, 2, 18, 99)
 
 if "dev" in __version__:
