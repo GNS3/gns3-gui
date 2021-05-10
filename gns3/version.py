3d07db5c5fb06cd08a370e1823f66454fc9c0931
grossmj <grossmj@gns3.net>
2021-05-10 23:44:40 +0930

    Development on 2.2.22dev1

--- gns3/version.py
+++ gns3/version.py
@@ -23,8 +23,8 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "2.2.21"
-__version_info__ = (2, 2, 21, 0)
+__version__ = "2.2.22dev1"
+__version_info__ = (2, 2, 22, 99)
 
 if "dev" in __version__:
     try:
