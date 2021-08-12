270301d9b53f612a4b7bb31f356f185d6edbb333
grossmj <grossmj@gns3.net>
2021-08-12 16:05:20 +0930

    Bump version to 3.0.0dev3

--- gns3/version.py
+++ gns3/version.py
@@ -23,7 +23,7 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "3.0.0dev2"
+__version__ = "3.0.0dev3"
 __version_info__ = (3, 0, 0, 99)
 
 if "dev" in __version__:
