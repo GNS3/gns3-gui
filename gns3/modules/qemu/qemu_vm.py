9016975958c2b55937c55b933143b39eda42f807
grossmj <grossmj@gns3.net>
2023-01-04 12:13:19 +0800

    Add Trusted Platform Module (TPM) support for Qemu VMs

--- gns3/modules/qemu/qemu_vm.py
+++ gns3/modules/qemu/qemu_vm.py
@@ -74,6 +74,7 @@ class QemuVM(Node):
                             "mac_address": QEMU_VM_SETTINGS["mac_address"],
                             "legacy_networking": QEMU_VM_SETTINGS["legacy_networking"],
                             "replicate_network_connection_state": QEMU_VM_SETTINGS["replicate_network_connection_state"],
+                            "tpm": QEMU_VM_SETTINGS["tpm"],
                             "create_config_disk": QEMU_VM_SETTINGS["create_config_disk"],
                             "platform": QEMU_VM_SETTINGS["platform"],
                             "on_close": QEMU_VM_SETTINGS["on_close"],
