7c56a2467cb830c0504ac215117e84f42fc39ad4
grossmj <grossmj@gns3.net>
2023-05-22 16:46:57 +0800

    Fix open IPv6 address for HTTP consoles. Fixes #3448

--- gns3/node.py
+++ gns3/node.py
@@ -17,6 +17,7 @@
 
 import os
 import pathlib
+import ipaddress
 import re
 
 from gns3.controller import Controller
@@ -678,6 +679,14 @@ class Node(BaseNode):
         host = self.settings()["console_host"]
         if host is None or host == "::" or host == "0.0.0.0" or host == "0:0:0:0:0:0:0:0":
             host = Controller.instance().host()
+        console_type = self.consoleType()
+        if console_type == "http" or console_type == "https":
+            try:
+                ip = ipaddress.ip_address(host)
+                if isinstance(ip, ipaddress.IPv6Address):
+                    host = '[' + host + ']'  # add brackets if it is IPv6 address
+            except ValueError:
+                pass
         return host
 
     def setStatus(self, status):
