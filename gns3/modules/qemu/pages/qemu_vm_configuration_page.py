dbfaa48ca90bd0bb83629577c63b0b87c7c490d2
grossmj <grossmj@gns3.net>
2024-04-04 18:43:31 +0700

    Fix retrieving value from uiQemuPathLineEdit

--- gns3/modules/qemu/pages/qemu_vm_configuration_page.py
+++ gns3/modules/qemu/pages/qemu_vm_configuration_page.py
@@ -653,7 +653,7 @@ class QemuVMConfigurationPage(QtWidgets.QWidget, Ui_QemuVMConfigPageWidget):
         else:
             settings["cpu_throttling"] = 0
         settings["process_priority"] = self.uiProcessPriorityComboBox.currentText().lower()
-        settings["qemu_path"] = self.QemuPathLineEdit.text().strip()
+        settings["qemu_path"] = self.uiQemuPathLineEdit.text().strip()
         settings["options"] = self.uiQemuOptionsLineEdit.text()
         settings["usage"] = self.uiUsageTextEdit.toPlainText()
         settings["tpm"] = self.uiTPMCheckBox.isChecked()
