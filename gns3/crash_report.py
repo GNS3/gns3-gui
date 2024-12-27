889d41636dfc87c7b5a8db246258dfcf46bff521
grossmj <grossmj@gns3.net>
2024-12-27 21:05:25 +0700

    Release v3.0.1

--- gns3/crash_report.py
+++ gns3/crash_report.py
@@ -50,7 +50,7 @@ class CrashReport:
     Report crash to a third party service
     """
 
-    DSN = "https://77c0749c7bf450b586f2495c285b10d2@o19455.ingest.us.sentry.io/38506"
+    DSN = "https://836426abf3e971acdf0e35f834874daf@o19455.ingest.us.sentry.io/38506"
     _instance = None
 
     def __init__(self):
--- gns3/version.py
+++ gns3/version.py
@@ -23,8 +23,8 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "3.0.0"
-__version_info__ = (3, 0, 0, 0)
+__version__ = "3.0.1"
+__version_info__ = (3, 0, 1, 0)
 
 if "dev" in __version__:
     try:
