69818705544a207c185d3a4461841a5b6a1a562a
grossmj <grossmj@gns3.net>
2026-05-08 17:01:14 +0800

    Update requirements.txt & python_requires

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     include_package_data=True,
     package_data={"gns3": ["configs/*.txt", "schemas/*.json"]},
     platforms="any",
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     setup_requires=["setuptools>=45.2"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
